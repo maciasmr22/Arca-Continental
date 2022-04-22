0610b65e45fee26ab48d75b6235586fe287f1389
LeonardoM24 <A00571960@itesm.mx>
2022-04-22 17:24:04 -0500

    jajajajaj XD

--- Arca Web/src/App.js
+++ Arca Web/src/App.js
@@ -1,70 +1,24 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Toolbar from './components/Toolbar/Toolbar';
+import App2 from './App2'
+import Login from "./components/Login";
+//import Tareas from './pages/Tareas/Tareas';
 
-import AboutUs from './pages/AboutUs/AboutUs';
-import Chat from './pages/Chat/Chat';
-import Entregas from './pages/Entregas/Entregas';
-import Inicio from './pages/Inicio/Inicio';
-import Perfil from './pages/Perfil/Perfil';
-import Ranking from './pages/Ranking/Ranking';
-import SideDrawer from './components/SideDrawer/SideDrawer';
-import Backdrop from './components/Backdrop/Backdrop';
-import Tareas from './pages/Tareas/Tareas';
-
-class App extends Component {
-  state = {
-    sideDrawerOpen: false
-  };
-
-  drawerToggleClickHandler = () => {
-    this.setState((prevState) => {
-      return {sideDrawerOpen: !prevState.sideDrawerOpen};
-    });
-  };
-
-  backdropClickHandler = () => {
-    this.setState({sideDrawerOpen: false});
-
-  };
-
-  render(){
-    let backdrop;
-
-    if (this.state.sideDrawerOpen) {
-      backdrop = <Backdrop click = {this.backdropClickHandler} />
-    }
-
-    return(
-      <div style = {{height: '100%'}}>
-        <Router>
-          <Toolbar drawerClickHandler = {this.drawerToggleClickHandler} />
-          <SideDrawer show = {this.state.sideDrawerOpen} />
-          {backdrop}
-          <main style={{marginTop: '5%'}}>
-          </main>
-          <Routes>
-            <Route exact path = '/' element = {<Inicio />} /> 
-
-            <Route path = '/profile' element = {<Perfil />} />
-
-            <Route path = '/entregas' element = {<Entregas />} />
-
-            <Route path = '/ranking' element = {<Ranking />} />
-
-            <Route path = '/chat' element = {<Chat />} /> 
-
-            <Route path = '/about-us' element = {<AboutUs />} />
-
-            <Route path= "/tareas" element = {<Tareas />} />
-
-          </Routes>
-        </Router>
+const App = () => {
+  const [log, setLog] = useState(true);
+  if (!log) {
+    return (
+      <div>
+        <Login />
       </div>
     );
   }
+  return (
+    <div>
+      <App2 />
+    </div>
+  );
 }
 
 export default App;
---
+++ Arca Web/src/App2.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Toolbar from './components/Toolbar/Toolbar';
+
+import AboutUs from './pages/AboutUs/AboutUs';
+import Chat from './pages/Chat/Chat';
+import Entregas from './pages/Entregas/Entregas';
+import Inicio from './pages/Inicio/Inicio';
+import Perfil from './pages/Perfil/Perfil';
+import Ranking from './pages/Ranking/Ranking';
+import SideDrawer from './components/SideDrawer/SideDrawer';
+import Backdrop from './components/Backdrop/Backdrop';
+//import Tareas from './pages/Tareas/Tareas';
+
+class App extends Component {
+  
+  state = {
+    sideDrawerOpen: false
+  };
+
+  drawerToggleClickHandler = () => {
+    this.setState((prevState) => {
+      return {sideDrawerOpen: !prevState.sideDrawerOpen};
+    });
+  };
+
+  backdropClickHandler = () => {
+    this.setState({sideDrawerOpen: false});
+
+  };
+
+  render(){
+    
+    let backdrop;
+
+    if (this.state.sideDrawerOpen) {
+      backdrop = <Backdrop click = {this.backdropClickHandler} />
+    }
+
+    return(
+      <div style = {{height: '100%'}}>
+        <Router>
+          <Toolbar drawerClickHandler = {this.drawerToggleClickHandler} />
+          <SideDrawer show = {this.state.sideDrawerOpen} />
+          {backdrop}
+          <main style={{marginTop: '5%'}}>
+          </main>
+          <Routes>
+            <Route exact path = '/' element = {<Inicio />} /> 
+
+            <Route path = '/profile' element = {<Perfil />} />
+
+            <Route path = '/entregas' element = {<Entregas />} />
+
+            <Route path = '/ranking' element = {<Ranking />} />
+
+            <Route path = '/chat' element = {<Chat />} /> 
+
+            <Route path = '/about-us' element = {<AboutUs />} />
+
+           {/* <Route path= "/tareas" element = {<Tareas />} />   */}
+
+          </Routes>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
