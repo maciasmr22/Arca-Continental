f8803d804a8df54cf436c950d814c1d6bfc7f3c4
AnaPaulaPedroza <a00830553@itesm.mx>
2022-04-26 12:19:38 -0500

    directorio

--- Arca Web/src/App.js
+++ Arca Web/src/App.js
@@ -5,7 +5,7 @@ import App2 from './App2'
 import Login from "./components/Login/Login";
 
 const App = () => {
-  const [token, setToken] = useState();
+  const [token, setToken] = useState(true);
 
   if(!token) {
     return <Login setToken={setToken} />
---
+++ Arca Web/src/pages Admin/Directorio/Directorio.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Directorio() {
+    return (
+        <div className="container">
+            <h1>Entregas</h1>
+            <table>
+                <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Planta</th>
+                    <th>Correo</th>
+                    <th>Entregas</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td/>
+                        <td/>
+                        <td/>
+                        <td/>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default Directorio;
