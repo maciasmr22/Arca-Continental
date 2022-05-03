4fc2d334c4c0a123b82430ce78dd92952531b6dd
LeonardoM24 <A00571960@itesm.mx>
2022-05-03 10:41:54 -0500

    gracias

---
+++ Arca Web/src/components/Directorio/Directorio.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function Directorio() {
+    return (
+        <div className="container">
+            <h1>Entregas</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Planta</th>
+                        <th>Correo</th>
+                        <th>Entrega</th>
+                        <th>Calificación</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td />
+                        <td />
+                        <td />
+                        <td />
+                        <td />
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default Directorio;
