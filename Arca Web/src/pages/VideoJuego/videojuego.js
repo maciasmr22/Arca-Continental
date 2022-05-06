ca1f27b74a9b666c3cc17b33553765a4d8160f64
Macias Romero <a00573134@itesm.mx>
2022-05-05 23:45:20 -0500

    así se siente México

---
+++ Arca Web/src/pages/VideoJuego/videojuego.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Unity, { UnityContext } from "react-unity-webgl";
+
+/*
+function videojuego() {
+
+    const unityContext = new UnityContext({
+    loaderUrl: "build/myunityapp.loader.js",
+    dataUrl: "build/myunityapp.data",
+    frameworkUrl: "build/myunityapp.framework.js",
+    codeUrl: "build/myunityapp.wasm",
+});
+
+    //VARIABLES
+
+    return (
+        //ES EL VIDEOJUEGO
+        <Unity unityContext={unityContext} />
+    );
+}
+*/
+function videojuego() {
+    return (
+        <div>videojuego</div>
+    )
+}
+export default videojuego
